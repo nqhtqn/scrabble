61179089fdac6fbdac1308aae07fcbdf9e6eecc5
Nath <97612424+nqhtqn@users.noreply.github.com>
2023-01-22 12:52:45 +0100

    Add files via upload

---
+++ header.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+void change(char chevalet[7], char pioche[102], int nbrLettre);
+
+#endif // HEADER_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "header.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
